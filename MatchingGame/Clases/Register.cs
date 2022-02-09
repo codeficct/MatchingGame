d8e8e02fc7dca761fb5003f40776a1ab85db4ba9
Luis Gabriel Janco <janco7249@gmail.com>
2022-02-09 08:25:21 -0400

    feat: add class to model user to create new user

---
+++ MatchingGame/Clases/Register.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatchingGame.Clases
+{
+    public class Register
+    {
+        public string name { get; set; }
+        public string photo { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string confirmPassword { get; set; }
+    }
+}
