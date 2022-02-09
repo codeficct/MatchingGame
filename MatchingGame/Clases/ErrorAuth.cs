d5cd410f6423909bd5289e20c5918e328294b870
Luis Gabriel Janco <janco7249@gmail.com>
2022-02-09 08:25:21 -0400

    feat: add class to model message to receive in ErrorAuth

---
+++ MatchingGame/Clases/ErrorAuth.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatchingGame.Clases
+{
+    public class ErrorAuth
+    {
+        public string message { get; set; }
+    }
+}
