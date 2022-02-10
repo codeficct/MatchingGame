16d9debe1d5129c5a4b18f78a89048c6e18b0a72
Luis Gabriel Janco <janco7249@gmail.com>
2022-02-10 08:25:21 -0400

    refactor: Provide parameter data to persist in the registry

--- MatchingGame/Vistas/LoginPage/AvatarsPage.xaml.cs
+++ MatchingGame/Vistas/LoginPage/AvatarsPage.xaml.cs
@@ -20,21 +20,24 @@ namespace MatchingGame.Vistas.LoginPage
             VerticalOptions = LayoutOptions.FillAndExpand,
             HorizontalOptions = LayoutOptions.FillAndExpand,
         };
-        string imageSelected = "https://i.ibb.co/DRd03qy/5.png";
+        string Name, Email, Password, ConfirmPassword, imageSelected;
         int rows = 8, columns = 2;
         public string[,] avatarsUrl = new string[,]
         {
             { "https://i.ibb.co/HF8xr9L/1.png", "https://i.ibb.co/p4g3kRT/2.png" },
             { "https://i.ibb.co/6t15gLB/3.png", "https://i.ibb.co/TqMsjx7/4.png" },
-            { "https://i.ibb.co/DRd03qy/5.png", "https://i.ibb.co/NFrG2hm/6.png"},
-            { "https://i.ibb.co/ZYgR13F/7.png", "https://i.ibb.co/YbSSK25/8.png"},
+            { "https://i.ibb.co/DRd03qy/5.png", "https://i.ibb.co/NFrG2hm/6.png" },
+            { "https://i.ibb.co/ZYgR13F/7.png", "https://i.ibb.co/YbSSK25/8.png" },
             { "https://i.ibb.co/nDdT1CX/9.png", "https://i.ibb.co/v3wDLZJ/10.png" },
             { "https://i.ibb.co/vDM18BM/11.png", "https://i.ibb.co/KrNPw32/12.png" },
             { "https://i.ibb.co/k3YmLRF/13.png", "https://i.ibb.co/XjyQcKz/14.png" },
             { "https://i.ibb.co/4Fkmr9D/15.png", "https://i.ibb.co/vHwhkyg/16.png " }
         };
-        public AvatarsPage()
+
+        public AvatarsPage(string name, string email, string password, string confirmPassword, string avatar = "https://i.ibb.co/DRd03qy/5.png")
         {
+            Name = name; Email = email; Password = password; ConfirmPassword = confirmPassword;
+            imageSelected = avatar;
             InitializeComponent();
             BindingContext = this;
             AvatarsContainer.Content = grid;
@@ -58,30 +61,17 @@ namespace MatchingGame.Vistas.LoginPage
                 {
                     ImageButton imageButton = new ImageButton { Source = avatarsUrl[r, c], Aspect = Aspect.AspectFit, BackgroundColor = Color.Transparent, BorderWidth = 6, BorderColor = Color.Transparent, CornerRadius = 1000, ClassId = avatarsUrl[r, c], HeightRequest = 140, WidthRequest = 140, Margin = 16, HorizontalOptions = LayoutOptions.Center };
                     grid.Children.Add(imageButton, c, r);
-                    imageButton.Clicked += async (sender, args) =>
+                    imageButton.Clicked += (sender, args) =>
                     {
-                        //((ImageButton)sender).BorderColor = Color.FromHex("#0099ff");
                         imageSelected = ((ImageButton)sender).ClassId;
                         DeselectAvatars();
-                        //imageButton.Source = ((ImageButton)sender).ClassId;
-                        //if (imageButton.ClassId != match && !isWin)
-                        //{
-                        //    if (Score < 0)
-                        //    {
-                        //        await App.Current.MainPage.Navigation.PushAsync(new GameOverPage(Score));
-                        //    }
-                        //    Score = Score - (Level * 5) - 10;
-                        //}
-                        //this.IsMatch(imageButton.ClassId);
-                        // await Task.Delay(1000);
-                        //  ((ImageButton)sender).Source = "question";
                     };
                 }
         }
 
         private async void SelectAvatar_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new RegisterPage(imageSelected));
+            await Navigation.PushAsync(new RegisterPage(Name, Email, Password, ConfirmPassword,imageSelected));
         }
 
         public void DeselectAvatars()
@@ -89,13 +79,9 @@ namespace MatchingGame.Vistas.LoginPage
             foreach (ImageButton item in grid.Children)
             {
                 if (imageSelected == item.ClassId)
-                {
                     item.BorderColor = Color.FromHex("#fe207d");
-                }
                 else
-                {
                     item.BorderColor = Color.Transparent;
-                }
             }
         }
 
