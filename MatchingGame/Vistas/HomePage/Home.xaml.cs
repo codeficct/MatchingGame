d0b083cd015c5f45f9336fa1e06c596265bfb1b2
Luis Gabriel Janco <janco7249@gmail.com>
2023-01-09 21:39:14 -0400

    feat: add home page

--- MatchingGame/Vistas/HomePage/Home.xaml.cs
+++ MatchingGame/Vistas/HomePage/Home.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MatchingGame.Vistas.GamePage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,11 @@ namespace MatchingGame.Vistas.HomePage
         public Home()
         {
             InitializeComponent();
+        }
+
+        private async void navigateToGame_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Game());
         }
     }
 }
