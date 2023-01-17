63c4c1b02fd1144f1b2379c1d951dc110a8806f0
Luis Gabriel Janco <janco7249@gmail.com>
2023-01-17 15:39:32 -0400

    feat: changes in GamePage

--- MatchingGame/Vistas/GamePage/Cards.xaml.cs
+++ MatchingGame/Vistas/GamePage/Cards.xaml.cs
@@ -14,33 +14,28 @@ namespace MatchingGame.Vistas.GamePage
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Cards : ContentView
     {
-        Matrix m1;
-        Grid grid1 = new Grid
-        {
-            VerticalOptions = LayoutOptions.FillAndExpand,
-            HorizontalOptions = LayoutOptions.FillAndExpand,
-        };
+        //Matrix m1;
+        //Grid grid1 = new Grid
+        //{
+        //    VerticalOptions = LayoutOptions.FillAndExpand,
+        //    HorizontalOptions = LayoutOptions.FillAndExpand,
+        //}; 
+        
         public Cards()
         {
-            grid1.RowDefinitions.Add(new RowDefinition());
-            grid1.RowDefinitions.Add(new RowDefinition());
-            grid1.RowDefinitions.Add(new RowDefinition());
+            //grid1.RowDefinitions.Add(new RowDefinition());
+            //grid1.RowDefinitions.Add(new RowDefinition());
+            //grid1.RowDefinitions.Add(new RowDefinition());
 
-            grid1.ColumnDefinitions.Add(new ColumnDefinition());
-            grid1.ColumnDefinitions.Add(new ColumnDefinition());
+            //grid1.ColumnDefinitions.Add(new ColumnDefinition());
+            //grid1.ColumnDefinitions.Add(new ColumnDefinition());
 
-            m1 = new Matrix();
-            m1.SetData(3, 2, 1);
-            m1.GenerateCards(ref grid1);
+            //m1 = new Matrix();
+            //m1.SetData(3, 2, 1);
+            //m1.GenerateCards(ref grid1);
 
-            Content = grid1;
+            //Content = grid1;
             InitializeComponent();
         }
-
-        //public void generateCard(ref Grid grid)
-        //{
-        //    m1.SetData(3, 2, 1);
-        //    m1.GenerateCards(ref grid1);
-        //}
     }
 }
--- MatchingGame/Vistas/GamePage/Game.xaml.cs
+++ MatchingGame/Vistas/GamePage/Game.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MatchingGame.Clases;
+using MatchingGame.Vistas.HomePage;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +15,102 @@ namespace MatchingGame.Vistas.GamePage
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Game : ContentPage
     {
-        public Game()
+        int currentLevel = 0, rows = 3, columns = 2;
+        public GameSetting Setting { get; set; }
+        Matrix m1;
+        Grid grid = new Grid
         {
+            VerticalOptions = LayoutOptions.FillAndExpand,
+            HorizontalOptions = LayoutOptions.FillAndExpand,
+        };
+        public Game(int level)
+        {
+            currentLevel = level;
+            Setting = new GameSetting
+            {
+                Level = currentLevel
+            };
+            BindingContext = Setting;
+            m1 = new Matrix();
+
             InitializeComponent();
+            CardContainer.Content = grid;
+            playGame.IsEnabled = true;
+            playGame.Opacity = 1;
+            Setting.Level = currentLevel;
+            BindingContext = Setting;
+            rows += currentLevel; columns += currentLevel;
+            m1.SetLevel(currentLevel);
+            generateNextLevel(true);
         }
 
         private async void btnClose_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PopAsync();
+            await Navigation.PushAsync(new Home());
+        }
+        private async void playGame_Clicked(object sender, EventArgs e)
+        {
+            bool isContinue = true;
+            int count = 0;
+            playGame.IsEnabled = false;
+            playGame.Opacity = 0.5;
+            MatchImageRandom.Source = "defaultcard";
+            Device.StartTimer(new TimeSpan(0, 0, 0, 0, 400), () =>
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    count++;
+                    grid.Children.Clear();
+                    StarGame();
+                    if (count == 10)
+                    {
+                        isContinue = false;
+                    }
+                });
+                return isContinue;
+            });
+
+            await Task.Delay(7000 + 7000 * (currentLevel / 5));
+            m1.HideCards(ref grid);
+            m1.SetMatchImage(ref MatchImageRandom);
+            //if (isPlaying)
+            //{
+            //    Setting.Level = currentLevel;
+            //    BindingContext = Setting;
+            //    rows += currentLevel; columns += currentLevel;
+            //    generateNextLevel();
+            //}
+            //isPlaying = true;
+        }
+
+        public void generateNextLevel(bool isDefault = false)
+        {
+            grid.RowDefinitions.Clear();
+            grid.ColumnDefinitions.Clear();
+            for (int r = 0; r < rows; r++)
+            {
+                grid.RowDefinitions.Add(new RowDefinition());
+            }
+            for (int c = 0; c < columns; c++)
+            {
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+            m1.SetData(rows, columns, 1);
+            if (isDefault)
+                m1.GenerateDefaultCards(ref grid);
+            else
+                m1.GenerateCards(ref grid);
+            if (CardContainer != null)
+            {
+                CardContainer.Content = grid;
+            }
+            // App.Current.MainPage.DisplayAlert("Message Context", Setting.GetLevel.ToString(), "cancel");
+        }
+
+        public void StarGame()
+        {
+            m1.Dispersion();
+            m1.GenerateCards(ref grid);
         }
     }
 }
