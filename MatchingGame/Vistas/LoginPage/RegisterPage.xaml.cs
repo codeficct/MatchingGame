99d095697ed60bdfff7430f408690213f8e4df42
Luis Gabriel Janco <janco7249@gmail.com>
2023-02-08 21:28:47 -0400

    refator: add functionality to button to navigate another page for select a avatar

--- MatchingGame/Vistas/LoginPage/RegisterPage.xaml.cs
+++ MatchingGame/Vistas/LoginPage/RegisterPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MatchingGame.Vistas.HomePage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,14 +13,31 @@ namespace MatchingGame.Vistas.LoginPage
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class RegisterPage : ContentPage
     {
-        public RegisterPage()
+        public RegisterPage(string avatar = "addProfile")
         {
             InitializeComponent();
+            SelectAvatar.Source = avatar;
         }
 
-        private void SelectAvatars_Clicked(object sender, EventArgs e)
+        private async void LogIn_Clicked(object sender, EventArgs e)
         {
+            await Navigation.PushAsync(new LoginPage());
+        }
 
+        protected override bool OnBackButtonPressed()
+        {
+            PromptForExit();
+            return true;
+        }
+
+        private async void PromptForExit()
+        {
+            await Navigation.PushAsync(new Home());
+        }
+
+        private async void SelectAvatar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AvatarsPage());
         }
     }
 }
