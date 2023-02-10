3c4c35499dca0e841771825aa4871d6454a4b8b8
Luis Gabriel Janco <janco7249@gmail.com>
2023-02-10 07:43:32 -0400

    feat: add Version object to model AppVersion

---
+++ MatchingGame/Clases/Version.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatchingGame.Clases
+{
+    internal class Version
+    {
+        public string _id { get; set; }
+        public string AppName { get; set; }
+        public string AppVersion { get; set; }
+        public string Developer { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string createdAt { get; set; }
+        public string updatedAt { get; set; }
+        public int __v { get; set; }
+    }
+}
