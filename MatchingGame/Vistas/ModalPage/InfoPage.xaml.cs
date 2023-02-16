8190bad6932ec17e71b303bba0cedc2238818cd8
Luis Gabriel Janco <janco7249@gmail.com>
2023-02-16 11:33:30 -0400

    feat: add info page as guide to game

---
+++ MatchingGame/Vistas/ModalPage/InfoPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MatchingGame.Vistas.ModalPage
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InfoPage : ContentPage
+    {
+        public InfoPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void CloseModal_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
